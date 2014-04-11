b9354b9b14f65bbfef75b3e409580e96539970c6
ceylog <wgfinalsin@gmail.com>
2014-04-11 17:59:18 +0800

    update

--- js/post.js
+++ js/post.js
@@ -212,4 +212,11 @@ $(document).ready(function(){
         $("head").append("<link rel='stylesheet' type='text/css' href='/css/css3-ani.css'/>");
         $.getScript('/js/css3-ani.js',function(){});
     }
+	
+	/* For zebra striping */
+	$("table tr:nth-child(odd)").addClass("odd-row");
+	/* For cell text alignment */
+	$("table td:first-child, table th:first-child").addClass("first");
+	/* For removing the last border */
+	$("table td:last-child, table th:last-child").addClass("last");
 });
